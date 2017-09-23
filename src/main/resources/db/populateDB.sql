33fdc3af1040c7a12df46bd299ee02b5e5c299b0
vasiliyeskin <eskin@rf.unn.ru>
2017-09-23 21:19:31 +0300

    Database initiolation and populate

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,7 +1,13 @@
 DELETE FROM user_roles;
 DELETE FROM users;
 DELETE FROM RESTAURANTS;
-ALTER SEQUENCE global_seq RESTART WITH 100000;
+DELETE FROM DISHES;
+ALTER SEQUENCE global_seq
+RESTART WITH 100000;
+ALTER SEQUENCE rest_seq
+RESTART WITH 200000;
+ALTER SEQUENCE dish_seq
+RESTART WITH 300000;
 
 INSERT INTO users (firstname, lastname, email, password)
 VALUES ('User', 'Smith', 'user@yandex.ru', 'password');
@@ -15,14 +21,46 @@ VALUES ('User2', 'Smith', 'user2@yandex.ru', 'password'),
   ('User4', 'Smith', 'user4@yandex.ru', 'password');
 
 INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', (SELECT id FROM USERS WHERE email='user@yandex.ru')),
-  ('ROLE_ADMIN', (SELECT id FROM USERS WHERE email='admin@gmail.com')),
-  ('ROLE_USER', (SELECT id FROM USERS WHERE email='admin@gmail.com')),
-  ('ROLE_USER', (SELECT id FROM USERS WHERE email='user2@yandex.ru')),
-  ('ROLE_USER', (SELECT id FROM USERS WHERE email='user3@yandex.ru')),
-  ('ROLE_USER', (SELECT id FROM USERS WHERE email='user4@yandex.ru'));
+  ('ROLE_USER', (SELECT id
+                 FROM USERS
+                 WHERE email = 'user@yandex.ru')),
+  ('ROLE_ADMIN', (SELECT id
+                  FROM USERS
+                  WHERE email = 'admin@gmail.com')),
+  ('ROLE_USER', (SELECT id
+                 FROM USERS
+                 WHERE email = 'admin@gmail.com')),
+  ('ROLE_USER', (SELECT id
+                 FROM USERS
+                 WHERE email = 'user2@yandex.ru')),
+  ('ROLE_USER', (SELECT id
+                 FROM USERS
+                 WHERE email = 'user3@yandex.ru')),
+  ('ROLE_USER', (SELECT id
+                 FROM USERS
+                 WHERE email = 'user4@yandex.ru'));
 
 INSERT INTO restaurants (title, address, email, site) VALUES
-  ('boltzmann''s restaurant', 'Nizhny Novgorod, 5 Gagarina ave.', 'boltzmann@mail.ru','boltzmann.ru' ),
-  ('maxvell''s restaurant', 'Nizhny Novgorod, 15 Gagarina ave.', 'maxvell@mail.ru','maxvell.ru' ),
-  ('feynman''s restaurant', 'Nizhny Novgorod, 7 Gagarina ave.', 'feynman@mail.ru','feynman.ru' );
+  ('boltzmann''s restaurant', 'Nizhny Novgorod, 5 Gagarina ave.', 'boltzmann@mail.ru', 'boltzmann.ru'),
+  ('maxvell''s restaurant', 'Nizhny Novgorod, 15 Gagarina ave.', 'maxvell@mail.ru', 'maxvell.ru'),
+  ('feynman''s restaurant', 'Nizhny Novgorod, 7 Gagarina ave.', 'feynman@mail.ru', 'feynman.ru');
+
+INSERT INTO dishes (name, datelunch, price, restaurant_id) VALUES
+  ('lobstar', now(), 10000, (SELECT id
+                             FROM RESTAURANTS
+                             WHERE email = 'maxvell@mail.ru')),
+  ('burger', now(), 2000, (SELECT id
+                           FROM RESTAURANTS
+                           WHERE email = 'maxvell@mail.ru')),
+  ('borsh', now(), 50000, (SELECT id
+                             FROM RESTAURANTS
+                             WHERE email = 'maxvell@mail.ru')),
+  ('lobstar', now(), 55000, (SELECT id
+                             FROM RESTAURANTS
+                             WHERE email = 'feynman@mail.ru')),
+  ('burger', now(), 2500, (SELECT id
+                           FROM RESTAURANTS
+                           WHERE email = 'feynman@mail.ru')),
+  ('borsh', now(), 30000, (SELECT id
+                           FROM RESTAURANTS
+                           WHERE email = 'boltzmann@mail.ru'));
