8fa39d4b8b82310509ea26ea678c4f1db1ee2720
vasiliyeskin <eskin@rf.unn.ru>
2017-09-24 14:27:34 -0600

    The edit of the database and database populating.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,7 +1,9 @@
 DELETE FROM user_roles;
+DELETE FROM dishesofday;
+DELETE FROM DISHES;
 DELETE FROM users;
 DELETE FROM RESTAURANTS;
-DELETE FROM DISHES;
+DELETE FROM VOTES;
 ALTER SEQUENCE global_seq
 RESTART WITH 100000;
 ALTER SEQUENCE rest_seq
@@ -40,27 +42,42 @@ INSERT INTO user_roles (role, user_id) VALUES
                  FROM USERS
                  WHERE email = 'user4@yandex.ru'));
 
-INSERT INTO restaurants (title, address, email, site) VALUES
-  ('boltzmann''s restaurant', 'Nizhny Novgorod, 5 Gagarina ave.', 'boltzmann@mail.ru', 'boltzmann.ru'),
-  ('maxvell''s restaurant', 'Nizhny Novgorod, 15 Gagarina ave.', 'maxvell@mail.ru', 'maxvell.ru'),
-  ('feynman''s restaurant', 'Nizhny Novgorod, 7 Gagarina ave.', 'feynman@mail.ru', 'feynman.ru');
+INSERT INTO restaurants (title, address, email, site, REGISTERED) VALUES
+  ('boltzmann''s restaurant', 'Nizhny Novgorod, 5 Gagarina ave.', 'boltzmann@mail.ru', 'boltzmann.ru', now()),
+  ('maxwell''s restaurant', 'Nizhny Novgorod, 15 Gagarina ave.', 'maxwell@mail.ru', 'maxwell.ru', now()),
+  ('feynman''s restaurant', 'Nizhny Novgorod, 7 Gagarina ave.', 'feynman@mail.ru', 'feynman.ru', now());
 
-INSERT INTO dishes (name, datelunch, price, restaurant_id) VALUES
-  ('lobstar', now(), 10000, (SELECT id
+INSERT INTO dishes (name, price, registered, restaurant_id) VALUES
+  ('lobster', 10000, now(), (SELECT id
                              FROM RESTAURANTS
-                             WHERE email = 'maxvell@mail.ru')),
-  ('burger', now(), 2000, (SELECT id
+                             WHERE email = 'maxwell@mail.ru')),
+  ('burger', 2000, now(), (SELECT id
                            FROM RESTAURANTS
-                           WHERE email = 'maxvell@mail.ru')),
-  ('borsh', now(), 50000, (SELECT id
-                             FROM RESTAURANTS
-                             WHERE email = 'maxvell@mail.ru')),
-  ('lobstar', now(), 55000, (SELECT id
+                           WHERE email = 'maxwell@mail.ru')),
+  ('borsh', 50000, now(), (SELECT id
+                           FROM RESTAURANTS
+                           WHERE email = 'maxwell@mail.ru')),
+  ('lobster', 55000, now(), (SELECT id
                              FROM RESTAURANTS
                              WHERE email = 'feynman@mail.ru')),
-  ('burger', now(), 2500, (SELECT id
+  ('burger', 2500, now(), (SELECT id
                            FROM RESTAURANTS
                            WHERE email = 'feynman@mail.ru')),
-  ('borsh', now(), 30000, (SELECT id
+  ('borsh', 30000, now(), (SELECT id
                            FROM RESTAURANTS
                            WHERE email = 'boltzmann@mail.ru'));
+
+INSERT INTO DISHESOFDAY (datelunch, DISH_ID) VALUES
+  (now(),(SELECT id FROM DISHES WHERE name = 'lobster' and RESTAURANT_ID=(SELECT id FROM RESTAURANTS WHERE email = 'maxwell@mail.ru'))),
+  (now(),(SELECT id FROM DISHES WHERE name = 'burger' and RESTAURANT_ID=(SELECT id FROM RESTAURANTS WHERE email = 'maxwell@mail.ru'))),
+  (now(),(SELECT id FROM DISHES WHERE name = 'borsh' and RESTAURANT_ID=(SELECT id FROM RESTAURANTS WHERE email = 'maxwell@mail.ru'))),
+  (now(),(SELECT id FROM DISHES WHERE name = 'lobster' and RESTAURANT_ID=(SELECT id FROM RESTAURANTS WHERE email = 'feynman@mail.ru'))),
+  (now(),(SELECT id FROM DISHES WHERE name = 'burger' and RESTAURANT_ID=(SELECT id FROM RESTAURANTS WHERE email = 'feynman@mail.ru'))),
+  (now(),(SELECT id FROM DISHES WHERE name = 'borsh' and RESTAURANT_ID=(SELECT id FROM RESTAURANTS WHERE email = 'boltzmann@mail.ru')));
+
+INSERT INTO votes(DATELUNCH, USER_ID, RESTAURANT_ID) VALUES
+  (now(),(SELECT id FROM USERS WHERE email = 'user@yandex.ru'),(SELECT id FROM RESTAURANTS WHERE email = 'maxwell@mail.ru')),
+  (now(),(SELECT id FROM USERS WHERE email = 'admin@gmail.com'),(SELECT id FROM RESTAURANTS WHERE email = 'maxwell@mail.ru')),
+  (now(),(SELECT id FROM USERS WHERE email = 'user2@yandex.ru'),(SELECT id FROM RESTAURANTS WHERE email = 'feynman@mail.ru')),
+  (now(),(SELECT id FROM USERS WHERE email = 'user3@yandex.ru'),(SELECT id FROM RESTAURANTS WHERE email = 'feynman@mail.ru')),
+  (now(),(SELECT id FROM USERS WHERE email = 'user4@yandex.ru'),(SELECT id FROM RESTAURANTS WHERE email = 'boltzmann@mail.ru'));
