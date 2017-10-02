35b265c684f8f6c4b32abd5777d5ffcf223e25fa
vasiliyeskin <eskin@rf.unn.ru>
2017-10-01 23:24:53 -0600

    Heroku dependeces were added

--- hr.bat
+++ hr.bat
@@ -1,2 +1,2 @@
 call mvn -B -s settings.xml -DskipTests=true clean package
-call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -jar target/dependency/webapp-runner.jar target/*.war
+call java -Dspring.profiles.active="jpa,heroku" -jar target/dependency/webapp-runner.jar target/*.war
